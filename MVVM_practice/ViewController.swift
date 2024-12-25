23b12c879b325642b461d37627ecbbd3b1589099
Ohjackson <wogus3673@naver.com>
2024-12-25 17:32:55 +0900

    Initial Commit

---
+++ MVVM_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM_practice
+//
+//  Created by Jaehyun Ahn on 12/25/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
